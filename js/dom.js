4a287844d87658811ab6eed29881f03fce767489
deepak104080 <deepak104080@gmail.com>
2022-04-20 00:06:17 +0530

    19th April 2022 - Session

--- js/dom.js
+++ js/dom.js
@@ -119,3 +119,96 @@ console.log(movies_cast);
 
 
 document.querySelectorAll('.movie span')[0].innerHTML = 'Ms ABC';
+
+
+// innerHTML/innerText/textContent - Access/Write content inside any element
+{/* <div>My name</div> */}
+
+let temp_div = document.createElement('div');
+console.log(temp_div);
+temp_div.innerText = "--------------My name------------------";
+console.log(temp_div);
+
+let temp_parent_div = document.getElementsByClassName('movie')[0];
+console.log(temp_parent_div);
+temp_parent_div.appendChild(temp_div);
+
+
+/* 
+<div class="travel">
+    <ul>
+        <li>Car</li>
+        <li>Bike</li>
+    </ul>
+</div>
+*/
+
+let temp_div_list = document.createElement('div');
+temp_div_list.setAttribute('class', 'travel');
+
+let temp_ul_list = document.createElement('ul');
+
+let temp_li_1_list = document.createElement('li');
+temp_li_1_list.innerHTML = 'Car';
+temp_ul_list.appendChild(temp_li_1_list);
+
+let temp_li_2_list = document.createElement('li');
+temp_li_2_list.innerHTML = 'Bike';
+temp_ul_list.appendChild(temp_li_2_list);
+
+temp_div_list.appendChild(temp_ul_list);
+console.log(temp_div_list);
+
+let body_temp = document.getElementsByTagName('body')[0];
+body_temp.appendChild(temp_div_list);
+
+//Append - appendChild, append, removechild, replaceChild
+//HW - use loop for multiple li creation
+
+// jQuery - Do not use - $
+document.getElementById('movie_name').style.color = 'red';
+
+document.getElementById('tagline').getAttribute('class');
+//HW - create element of img type and add image to it and display in movie element.
+
+
+let temp = document.querySelector('.movie');
+temp.childNodes
+temp.children
+
+
+
+
+// DOM Event Methods- click, blur, change, hover, focus, focusin, focusout, select, scroll, mouseover, keyup, keydown, keypress, touch, slide
+
+// inline function in html
+// add event listener
+
+
+// DOM Event - Object
+// event.target
+
+
+function display_name(event) {
+    console.log(event);
+    console.log(event.target);
+    //alert('My name is Deepak.')
+}
+
+function display_name_input() {
+    let temp = document.getElementById('your_name').value;
+    console.log(temp);
+    //alert('My name is - ' + temp);
+}
+
+function select_fn() {
+    console.log('selected...')
+}
+
+
+function temp_fn (event) {
+    console.log(event.target);
+    console.log(event.target.innerHTML);
+}
+
+//HW - how to read checkbox, radio
